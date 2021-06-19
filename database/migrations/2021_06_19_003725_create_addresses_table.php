a14cb61c5921825deaec57469af0d99107204f06
Berk Soroğlu <berksoroglu@gmail.com>
2021-06-19 03:40:00 +0300

    Migration ile db yapısının ayarlanması

---
+++ database/migrations/2021_06_19_003725_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('person_id');
+            $table->text('address');
+            $table->text('post_code');
+            $table->text('city_name');
+            $table->text('country_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
